f8f47079afffc2b5d65031e139d6852f537dd28b
vahid2104 <140729262+vahid2104@users.noreply.github.com>
2026-07-26 09:21:50 +0200

    Update server to use HOST and parse PORT as number

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,8 +3,9 @@ import app from "./app";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
+const HOST = "0.0.0.0";
 
-app.listen(PORT, () => {
-  console.log(`Backend server is running on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Backend server is running on port ${PORT}`);
 });
